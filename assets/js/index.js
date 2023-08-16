f323f7202fed388ad4ae34ea9789067c7b978ae1
Matheus Sampaio Soier <132232562+Matheus-soier@users.noreply.github.com>
2023-08-15 22:56:30 -0300

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,29 @@
 let inputCvc = document.querySelector('.numeroInputCvc');
+let outputCvc = document.querySelector('#outputCvc')
 let inputValMm = document.getElementById('cardDataExMM');
 let inputValYy = document.getElementById('cardDataExYY');
 let inputCardNumber = document.querySelector('#cardNumber');
+let outputCardNumber = document.querySelector('#numero-card');
+let inputNameCard = document.querySelector('#cardName');
+let outputNameCard = document.querySelector('#nome-card');
+
+inputNameCard.addEventListener('input', function () {
+  if (inputNameCard.value.length > 0) {
+    outputNameCard.textContent = inputNameCard.value
+  } else {
+    outputNameCard.textContent = 'JANE APPLESEED'
+  }
+})
 
 inputCvc.addEventListener('input', function () {
   var maxLengthCvc = 3;
   if (inputCvc.value.length > maxLengthCvc) {
     inputCvc.value = inputCvc.value.slice(0, maxLengthCvc);
+  }
+  if (inputCvc.value.length > 0) {
+    outputCvc.textContent = inputCvc.value;
+  } else {
+    outputCvc.textContent = '000'
   }
 });
 
@@ -29,4 +46,24 @@ inputCardNumber.addEventListener('input', function () {
   if (inputCardNumber.value.length > maxLengthVal) {
     inputCardNumber.value = inputCardNumber.value.slice(0, maxLengthVal)
   }
+
+  if (inputCardNumber.value.length > 0) {
+    outputCardNumber.textContent = inputCardNumber.value
+  } else {
+    outputCardNumber.textContent = '0000 0000 0000 0000'
+  }
 });
+
+function concatenaValidade() {
+  let validadeMM = document.querySelector('#cardDataExMM').value;
+  let validadeYY = document.querySelector('#cardDataExYY').value;
+  let validadeMMeYY = (`${validadeMM}/${validadeYY}`);
+  let outputvalidade = document.querySelector('#validade-card');
+  
+  if (validadeMM > 0) {
+    outputvalidade.textContent = validadeMMeYY
+  } else {
+    outputvalidade.textContent = '00/00'
+  }
+}
+
